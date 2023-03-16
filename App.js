a66fe5b3ca703524e9def292265bb80b4b072b45
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-03-16 16:24:48 +0300

    Handle the page's navigation
    create a variable AppNavigator and initialize it to createStackNavigator from react-navigation
    inside the object declare route name as the property and the component it navigates to as the value
    set an initial route for the page
    wrap the container in a Provider and pass the store

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import  Example from './example/ScreenA'
+// export default Example
 
-export default function App() {
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from 'react-navigation-stack';
+import React from 'react';
+import { StyleSheet,View} from 'react-native';
+import Constants from 'expo-constants';
+import contacts,{compareNames}  from './contacts'
+import AddContactScreen from "./screens/AddContactScreen";
+import ContactListScreen from "./screens/ContactListScreen";
+import ContactDetailsScreen from "./screens/ContactDetailsScreen"
+import { fetchUsers } from "./Api";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+const  AppNavigator = createStackNavigator({
+         AddContact:AddContactScreen,
+         ContactList: ContactListScreen,
+        ContactDetails:ContactDetailsScreen
+},
+{
+    initialRouteName: "ContactList"
+})
+
+const AppContainer = createAppContainer(AppNavigator);
+export default class App extends React.Component {
+  state={
+    showContacts: true,
+    contacts:[],
+  }
+
+
+
+  addContact = (newContact) => {
+    this.setState(prevState=>({
+      contacts:[...prevState.contacts,newContact],
+    }))
+  }
+  render(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+    <AppContainer
+        screenProps={{
+            contacts:this.state.contacts,
+            addContact:this.addContact
+        }}
+    />
+    </Provider>
   );
+}
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding:Constants.statusBarHeight,
   },
+  
 });
+
